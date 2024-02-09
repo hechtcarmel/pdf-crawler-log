745c91db96fdea873acb868d6a7909560e7ff4fb
carmelhe <carmelhe@rafael.co.il>
2024-02-09 17:25:06 +0200

    first

---
+++ src/main/java/com/gil/pdfscrapper/PdfScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.gil.pdfscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfScrapperApplication.class, args);
+	}
+
+}
